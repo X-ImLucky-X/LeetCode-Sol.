b4ec162148d637354f90baa9344eda9a822ea9fc
X-ImLucky-X <136382281+X-ImLucky-X@users.noreply.github.com>
2026-07-06 01:34:00 +0530

    Time: 0 ms (100%), Space: 27.4 MB (90.5%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,33 +1,36 @@
+
 class Solution {
-    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
 public:
+    int maxx = 0;
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ROWS = grid.size(), COLS = grid[0].size();
         int area = 0;
 
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (grid[r][c] == 1) {
-                    area = max(area, dfs(grid, r, c));
+        int rows=grid.size();
+        int cols=grid[0].size();
+
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c]==1){
+                    dfs(grid,r,c,area);
+
+                    maxx = max(area,maxx);
+                    area = 0;
                 }
             }
         }
-
-        return area;
+        return maxx;
     }
-
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.size() ||
-            c >= grid[0].size() || grid[r][c] == 0) {
-            return 0;
+private:
+    void dfs(vector<vector<int>>& grid,int r,int c,int& area){
+        if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]==0){
+                        
+            return;
         }
-
-        grid[r][c] = 0;
-        int res = 1;
-        for (int i = 0; i < 4; i++) {
-            res += dfs(grid, r + directions[i][0],
-                             c + directions[i][1]);
+        grid[r][c]=0;
+        area++;
+        for(int i=0;i<4;i++){
+            dfs(grid,r+dir[i][0],c+dir[i][1],area);
         }
-        return res;
     }
 };
