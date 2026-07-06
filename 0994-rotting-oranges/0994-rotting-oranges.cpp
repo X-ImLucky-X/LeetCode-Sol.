3f9c12d35d875ba7c3beafb17bb0c9d23d746bce
X-ImLucky-X <136382281+X-ImLucky-X@users.noreply.github.com>
2026-07-06 22:53:23 +0530

    Time: 4 ms (14.58%), Space: 18 MB (5.56%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> q;
-        int fresh = 0;
-        int time = 0;
-
-        for (int r = 0; r < grid.size(); r++) {
-            for (int c = 0; c < grid[0].size(); c++) {
-                if (grid[r][c] == 1) {
-                    fresh++;
+        queue<pair<int,int>> visited;
+        int fresh=0;
+        int time=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    visited.push({i,j});
                 }
-                if (grid[r][c] == 2) {
-                    q.push({r, c});
+                if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
 
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (fresh > 0 && !q.empty()) {
-            int length = q.size();
-            for (int i = 0; i < length; i++) {
-                auto curr = q.front();
-                q.pop();
-                int r = curr.first;
-                int c = curr.second;
-
-                for (const auto& dir : directions) {
-                    int row = r + dir.first;
-                    int col = c + dir.second;
-                    if (row >= 0 && row < grid.size() &&
-                        col >= 0 && col < grid[0].size() &&
-                        grid[row][col] == 1) {
-                        grid[row][col] = 2;
-                        q.push({row, col});
+        vector<vector<int>> dir={{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while(!visited.empty() && fresh>0){
+            int size=visited.size();
+            while(size--){
+                auto curr=visited.front();
+                visited.pop();
+                int rows=curr.first;
+                int cols=curr.second;
+                for(auto x:dir){
+                    int newrows=rows+x[0];
+                    int newcols=cols+x[1];
+                    if(newrows >= 0 && newrows < grid.size() && newcols >= 0 && newcols < grid[0].size() && grid[newrows][newcols] == 1){
+                        grid[newrows][newcols]=2;
+                        visited.push({newrows,newcols});
                         fresh--;
                     }
                 }
             }
             time++;
         }
-        return fresh == 0 ? time : -1;
+        return (fresh==0?time:-1);
     }
 };
