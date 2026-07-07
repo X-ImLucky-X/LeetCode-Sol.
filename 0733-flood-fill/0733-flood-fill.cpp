a86e00ff278f44928c2fee8cd48ae0aced7a7aa3
X-ImLucky-X <136382281+X-ImLucky-X@users.noreply.github.com>
2026-07-07 22:39:32 +0530

    Time: 7 ms (1.92%), Space: 19.3 MB (8.75%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    void dfs(int sr, int sc, vector<vector<int>>& image, int color, int initColor){
+        image[sr][sc] = color;
+        vector<vector<int>> dir = {{1,  0}, {-1, 0},  {0, 1}, {0, -1}};
+        for(auto x: dir){
+            int dr = sr + x[0];
+            int dc = sc + x[1];
+            if( dr >= 0 && dr<image.size() && dc >= 0 && dc<image[0].size() && image[dr][dc] == initColor)
+                 dfs(dr, dc, image, color, initColor);
+        }
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initColor = image[sr][sc];
+        if(initColor == color) return image;
+        dfs(sr, sc, image, color, initColor);
+
+        return image;
+    }
+};
