92f54358a11d64d8f4dd31f94f774bd8235ae36c
X-ImLucky-X <136382281+X-ImLucky-X@users.noreply.github.com>
2026-07-10 11:14:00 +0530

    Time: 260 ms (5.02%), Space: 66.2 MB (5.06%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> dir={
+        {1,0},{-1,0},{0,1},{0,-1}
+    };
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==1)dfs(i,0,vis,grid);
+            if(grid[i][n-1]==1)dfs(i,n-1,vis,grid);
+        }
+        for(int i=0;i<n;i++){
+            if(grid[0][i]==1)dfs(0,i,vis,grid);
+            if(grid[m-1][i]==1)dfs(m-1,i,vis,grid);
+        }
+
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1)count++;
+            }
+        }
+        return count;
+    }
+private:
+    void dfs(int r,int c,vector<vector<bool>>& vis,vector<vector<int>>& grid){
+        if(grid[r][c]==1 && !vis[r][c]){
+            grid[r][c]=0;
+        }
+        for(auto x:dir){
+            int nr=r+x[0];
+            int nc=c+x[1];
+            if(nr>=0 && nc>=0 && nr<grid.size() && nc<grid[0].size() && grid[nr][nc]==1){
+                dfs(nr,nc,vis,grid);
+            }
+        }
+    }
+};
