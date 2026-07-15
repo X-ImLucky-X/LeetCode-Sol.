3c8368f03cf59567d10a21086698451840e0ff39
X-ImLucky-X <136382281+X-ImLucky-X@users.noreply.github.com>
2026-07-15 21:01:44 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.94%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int xorr = n;
-        for (int i = 0; i < n; i++) {
-            xorr ^= i ^ nums[i];
+        int xorr=nums.size();
+        for(int i=0;i<nums.size();i++){
+            xorr^=i^nums[i];
         }
         return xorr;
     }
