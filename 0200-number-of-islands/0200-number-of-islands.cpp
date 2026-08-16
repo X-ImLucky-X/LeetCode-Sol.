e0aea97ede3b7388fe68aef8b8184579b28b9293
Lakshya Kumar Singh <136382281+X-ImLucky-X@users.noreply.github.com>
2026-08-16 10:46:35 +0530

    Time: 25 ms (71.86%), Space: 16.4 MB (70.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,49 @@
 class Solution {
-    int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+    int dir[4][2] = {
+        {1, 0},
+        {-1, 0},
+        {0, 1},
+        {0, -1}
+    };
+
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int rows=grid.size();
-        int cols=grid[0].size();
-        int islands=0;
-        for(int r=0;r<rows;r++){
-            for(int c=0;c<cols;c++){
-                if(grid[r][c]=='1'){
-                    dfs(grid,r,c);
-                    islands++;
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        int count = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                if (grid[r][c] == '1') {
+                    count++;
+
+                    dfs(grid, r, c);
                 }
             }
         }
-        return islands;
+
+        return count;
     }
+
 private:
-    void dfs(vector<vector<char>>& grid,int r,int c){
-        if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]=='0'){
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+
+        if (r < 0 || c < 0 ||
+            r >= grid.size() ||
+            c >= grid[0].size() ||
+            grid[r][c] == '0') {
             return;
         }
-        grid[r][c]='0';
-        for(int i=0;i<4;i++){
-            dfs(grid,r+dir[i][0],c+dir[i][1]);
+
+        // Mark as visited
+        grid[r][c] = '0';
+
+        for (int i = 0; i < 4; i++) {
+            dfs(grid,
+                r + dir[i][0],
+                c + dir[i][1]);
         }
     }
 };
