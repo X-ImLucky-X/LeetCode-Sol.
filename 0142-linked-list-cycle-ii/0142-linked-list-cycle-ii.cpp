d679a6fc936ba93316259776756090f783e783b5
Lakshya Kumar Singh <136382281+X-ImLucky-X@users.noreply.github.com>
2026-08-16 09:28:18 +0530

    Time: 13 ms (6.36%), Space: 13.8 MB (6.93%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,6 +9,14 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        unordered_set<ListNode*> check;
+        return find(head,check);
+    }
+private:
+    ListNode* find(ListNode* node,unordered_set<ListNode*>& check){
+        if(!node) return nullptr;
+        if(check.find(node)!=check.end())return node;
+        check.insert(node);
+        return find(node->next,check);
     }
 };
