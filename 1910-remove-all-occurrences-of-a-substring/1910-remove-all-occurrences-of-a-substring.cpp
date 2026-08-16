7256aef0a5daacdabaa79b880f07bed90175c347
Lakshya Kumar Singh <136382281+X-ImLucky-X@users.noreply.github.com>
2026-08-16 08:28:18 +0530

    Time: 3 ms (32.35%), Space: 12.2 MB (9.86%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string ans;
+        int m = part.size();
+
+        for (char c : s) {
+            ans += c;
+
+            if (ans.size() >= m &&
+                ans.substr(ans.size() - m) == part) {
+                ans.erase(ans.size() - m);
+            }
+        }
+
+        return ans;
+    }
+};
